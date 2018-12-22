cb2b1e75425b9428c5b65d5eb0c982961da7e709
Cory O'Brien <cory@prtcl.cc>
2018-12-21 19:14:23 -0500

    icon and slider tweaks

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import IosArrowBack from 'react-ionicons/lib/IosArrowBack';
 import IosArrowForward from 'react-ionicons/lib/IosArrowForward';
@@ -22,18 +22,49 @@ const TYPES = {
   refresh: IosRefresh,
 };
 
-const Icon = ({ color = 'white', onClick, size = SIZE, type, ...props }) => {
-  const Type = TYPES[type];
+export default class Icon extends Component {
+  state = {
+    isActive: false,
+  };
 
-  return (
-    <a className={stylesheet.container} onClick={onClick}>
-      <Type
-        {...props}
-        className={classNames([stylesheet.icon, COLORS[color]])}
-        fontSize={`${size}px`}
-      />
-    </a>
-  );
-};
+  static defaultProps = {
+    color: 'white',
+    size: SIZE,
+  };
 
-export default Icon;
+  handleMouseDown = () => {
+    const { onClick } = this.props;
+
+    this.setState({ isActive: true });
+    onClick();
+  };
+
+  handleMouseUp = () => {
+    this.setState({ isActive: false });
+  };
+
+  render () {
+    const { color, size, type, ...props } = this.props;
+    const { isActive } = this.state;
+
+    const Type = TYPES[type];
+    const fontSize = `${size}px`;
+    const classes = classNames([
+      stylesheet.container,
+      COLORS[color],
+      isActive && stylesheet.isActive,
+    ]);
+
+    return (
+      <a
+        className={classes}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        onTouchEnd={this.handleMouseUp}
+        onTouchStart={this.handleMouseDown}
+      >
+        <Type {...props} className={stylesheet.icon} fontSize={fontSize} />
+      </a>
+    );
+  }
+}
