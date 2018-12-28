fb67008da7db741b2b3478e9386b7f395b684638
Cory O'Brien <cory@prtcl.cc>
2018-12-28 04:26:47 -0500

    recorder keyboard shortcuts

--- src/components/Recorder/Recorder.hoc.js
+++ src/components/Recorder/Recorder.hoc.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import compose from '../../lib/compose';
+import Mousetrap from '../../lib/mousetrap';
 import withSynthesisEngine from '../../hoc/withSynthesisEngine';
 import { MODES } from './lib/constants';
+
+const SHORTCUTS = {
+  TOGGLE_RECORD: 'space',
+};
 
 const withMediaRecorder = () => Comp =>
   class MediaRecorder extends Component {
     state = {
       recordingMode: MODES.EMPTY,
+    };
+
+    constructor (props) {
+      super(props);
+
+      this.mousetrap = new Mousetrap();
+      this.mousetrap.bind(SHORTCUTS.TOGGLE_RECORD, this.toggleRecord);
+    }
+
+    toggleRecord = () => {
+      const { recordingMode } = this.state;
+
+      if (recordingMode === MODES.EMPTY) {
+        this.record();
+      }
+
+      if (
+        recordingMode === MODES.RECORDING
+        || recordingMode === MODES.PLAYING
+      ) {
+        this.pause();
+      }
+
+      if (recordingMode === MODES.PAUSED) {
+        this.play();
+      }
     };
 
     setRecordingMode = recordingMode => {
